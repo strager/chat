ab26fe75af9852fe781ad1ca289195cecefee0d7
Matthew Glazar <strager.nds@gmail.com>
2020-03-13 18:16:39 -0700

    Send message given as command argument

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-let fs = require("fs");
-let tmi = require("tmi.js");
+main();
 
-let SECRET = fs.readFileSync('SECRET', 'utf-8').trim();
+function main() {
+    let args = process.argv.slice(2);
+    if (args.length !== 1) {
+        console.error('error: expected exactly one argument');
+        console.error(`usage: ${process.argv[1]} MESSAGE_TO_SEND`);
+        process.exit(1);
+    }
 
-let client = new tmi.Client({
-	connection: {
-		reconnect: false,
-		secure: true
-	},
-	identity: {
-		username: 'strager',
-		password: `oauth:${SECRET}`,
-	},
-});
+    let messageToSend = args[0];
+    sendMessage(messageToSend)
+}
 
-client.connect();
-client.on('connected', () => {
-    client.say('#strager', 'hello world');
-    client.disconnect();
-})
+function sendMessage(messageToSend) {
+    let fs = require("fs");
+    let tmi = require("tmi.js");
+
+    let SECRET = fs.readFileSync('SECRET', 'utf-8').trim();
+    let client = new tmi.Client({
+            connection: {
+                    reconnect: false,
+                    secure: true
+            },
+            identity: {
+                    username: 'strager',
+                    password: `oauth:${SECRET}`,
+            },
+    });
+
+    client.connect();
+    client.on('connected', () => {
+        client.say('#strager', messageToSend);
+        client.disconnect();
+    })
+}
