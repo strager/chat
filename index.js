842513e972f51ced8f1514c713453d0121d5a1fb
Matthew Glazar <strager.nds@gmail.com>
2020-03-13 18:13:42 -0700

    Say hello then exit

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
 let fs = require("fs");
 let tmi = require("tmi.js");
 
-let secret = fs.readFileSync('SECRET', 'utf-8').trim();
+let SECRET = fs.readFileSync('SECRET', 'utf-8').trim();
 
 let client = new tmi.Client({
-	//options: { debug: true },
 	connection: {
 		reconnect: false,
 		secure: true
 	},
 	identity: {
 		username: 'strager',
-		password: `oauth:${secret}`,
+		password: `oauth:${SECRET}`,
 	},
-	//channels: [ 'strager' ]
 });
 
 client.connect();
-client.on('message', (channel, tags, message, self) => {
-    if(self) return;
-    try {
-        client.say(channel, `@${tags.username}, heya!`);
-    } catch (error) {
-        console.error(error.stack);
-    }
-});
+client.on('connected', () => {
+    client.say('#strager', 'hello world');
+    client.disconnect();
+})
