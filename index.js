33a7b139bdf17aea5f338f5f93532fabf439396f
Matthew Glazar <strager.nds@gmail.com>
2020-03-13 18:18:24 -0700

    Format code with Prettier

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
+#!/usr/bin/env node
+
 main();
 
 function main() {
-    let args = process.argv.slice(2);
-    if (args.length !== 1) {
-        console.error('error: expected exactly one argument');
-        console.error(`usage: ${process.argv[1]} MESSAGE_TO_SEND`);
-        process.exit(1);
-    }
+  let args = process.argv.slice(2);
+  if (args.length !== 1) {
+    console.error("error: expected exactly one argument");
+    console.error(`usage: ${process.argv[1]} MESSAGE_TO_SEND`);
+    process.exit(1);
+  }
 
-    let messageToSend = args[0];
-    sendMessage(messageToSend)
+  let messageToSend = args[0];
+  sendMessage(messageToSend);
 }
 
 function sendMessage(messageToSend) {
-    let fs = require("fs");
-    let tmi = require("tmi.js");
+  let fs = require("fs");
+  let tmi = require("tmi.js");
 
-    let SECRET = fs.readFileSync('SECRET', 'utf-8').trim();
-    let client = new tmi.Client({
-            connection: {
-                    reconnect: false,
-                    secure: true
-            },
-            identity: {
-                    username: 'strager',
-                    password: `oauth:${SECRET}`,
-            },
-    });
+  let SECRET = fs.readFileSync("SECRET", "utf-8").trim();
+  let client = new tmi.Client({
+    connection: {
+      reconnect: false,
+      secure: true
+    },
+    identity: {
+      username: "strager",
+      password: `oauth:${SECRET}`
+    }
+  });
 
-    client.connect();
-    client.on('connected', () => {
-        client.say('#strager', messageToSend);
-        client.disconnect();
-    })
+  client.connect();
+  client.on("connected", () => {
+    client.say("#strager", messageToSend);
+    client.disconnect();
+  });
 }
